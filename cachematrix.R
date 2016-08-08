c5e6284a5ca89abfcd03b5757466779e9e780686
lamontagnea <lamontagnea@gmail.com>
2016-08-07 20:33:19 -0700

    committing with all comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,20 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first function here returns a list of functions to the parent environment and 
+## also includes objects x and s, x being a matrix and s being the inverse of the matrix.
+## The second function, cacheSolve, then checks to see if there's a cached version of s.
+## If there is, it returns it with a helpful message. If not, it returns a new value for the inverse of 
+## the matrix
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
         s <- NULL 
-        set <- function(y) { 
-                x <<- y 
-                s <<- NULL 
+        set <- function(y) { #sets both data objects x and s within the parent enviroments
+                x <<- y #assigns y to x in parent enviro
+                s <<- NULL #assigns NULL to s in parent environment so when x is reset, s is cleared
         }
-        get <- function() x 
-        setsolve <- function(solve) s <<- solve 
-        getsolve <- function() s 
-        list(set = set, get = get,
+        get <- function() x #retrieves the matrix x
+        setsolve <- function(solve) s <<- solve #assigns solve fcn to s object in parent environment
+        getsolve <- function() s # retrieves the solve function
+        list(set = set, get = get, #names the functions for use with the handy $ operator
              setsolve = setsolve,
              getsolve = getsolve)
 
@@ -20,19 +22,20 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 
-
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x) {
         
         
-        s <- x$getsolve()
+        s <- x$getsolve() #assigns s the value of x$getsolve(), which may be null
         if(!is.null(s)) { 
                 message("Get ALL the cached things!")
-                return(s)
+                return(s) #if x$getsolve() is not null, then lreturn the cached item
         }
-        data <- x$get() 
-        s <- solve(data, ...)
-        x$setsolve(s)
-        s
+        
+        
+        data <- x$get() #otherwise, get the value of x$get(), which retrieves the matrix x
+        s <- solve(data) #execute solve on data and assign it to s
+        x$setsolve(s) #execute the x$setsolve function on s
+        s #return s
         
 }
 
