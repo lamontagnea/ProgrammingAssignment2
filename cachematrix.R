648fd77950b3762faa284b9ee791228ed8f9b0b0
lamontagnea <lamontagnea@gmail.com>
2016-08-07 19:24:29 -0700

    Committing solution without commented markup

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,40 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        s <- NULL 
+        set <- function(y) { 
+                x <<- y 
+                s <<- NULL 
+        }
+        get <- function() x 
+        setsolve <- function(solve) s <<- solve 
+        getsolve <- function() s 
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 
 }
 
 
-## Write a short comment describing this function
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        
+        s <- x$getsolve()
+        if(!is.null(s)) { 
+                message("Get ALL the cached things!")
+                return(s)
+        }
+        data <- x$get() 
+        s <- solve(data, ...)
+        x$setsolve(s)
+        s
+        
 }
+
+# Use the following to test the functions. 
+# source("cachematrix.R")
+# x <- matrix(1:4,2,2)
+# z <- makeCacheMatrix(x)
+# cacheSolve(z)
